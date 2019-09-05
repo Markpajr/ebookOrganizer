4713c6725b450442228d680f66702b4667a5bbbf
Markpajr <Shadowstar88@Hotmail.com>
2019-09-04 22:10:22 -0400

    Added comments

--- ebookOrg.py
+++ ebookOrg.py
@@ -4,25 +4,33 @@
 
 import glob, os, shutil
 
+# Sets working directory to where my ebook files are
 os.chdir(r'C:\Users\Owner\Downloads\Python Materials\Python - humble bundle')
 
+# Uses glob to get a list of every file in the directory
 allfiles = glob.glob('./*.*')
+# Removes .// from the glob strings in the list
 noprefix =[x[2:] for x in allfiles]
 noExt = []
+# Removes the extensions frome every file
 for i in noprefix:
     name = os.path.splitext(i)[0]
     noExt.append(name)
+# Removes all duplicates from the list by converting to a dict    
 uniFiles = list(dict.fromkeys(noExt))
 
+# Changes working directory to where I want the ebooks located
 os.chdir(r'C:\Users\Owner\Downloads\Python Materials\Python - humble bundle\test')
 
+# Creates folders for each ebook based on it's unique name
 for file in uniFiles:
     try:
         os.mkdir(file)
         print('Folder ', file, 'Created')
     except FileExistsError:
         print(file, ' Folder Exists')
-
+# Uses glob to make a list containing every file type for each unique ebook
+# Copies every extension for each ebook to their respective folders
 for file in uniFiles:
     filepath = glob.glob(f'../{file}.*')
     for xpath in filepath:
